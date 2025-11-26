43197be36b3aec8e2b0086b28a02066dc59885ad
Rachael Kenny <157695390+crn3@users.noreply.github.com>
2025-11-26 11:14:43 +0000

    Feat: add logging to CreatePerson

--- HelloWorldCode/HelloWorldCode/src/main/java/main/CreatePerson.java
+++ HelloWorldCode/HelloWorldCode/src/main/java/main/CreatePerson.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.Scanner;
 
 /**
@@ -8,6 +10,10 @@ import java.util.Scanner;
 
 public final class CreatePerson {
 
+    // Creating a logger
+    private static final Logger LOGGER =
+    Logger.getLogger(CreatePerson.class.getName());
+    
     // Private constructor for utility class
     private CreatePerson() { }
 
@@ -18,6 +24,9 @@ public final class CreatePerson {
      * @param args command-line arguments (not used)
      */
     public static void main(final String[] args) {
+
+        LOGGER.info("Begin application.");
+
         final Scanner scanner = new Scanner(System.in);
         String name;
         int age;
@@ -25,21 +34,30 @@ public final class CreatePerson {
         System.out.print("Enter name: ");
         name = scanner.nextLine();
         while (!name.matches("[a-zA-Z ]+")) {
+            LOGGER.log(Level.WARNING, "Invalid name input: {0}", name);
             System.out.print("Enter a valid name: ");
             name = scanner.nextLine();
         }
+        LOGGER.info("Valid name entered.");
         System.out.print("Enter age: ");
         while (!scanner.hasNextInt()) {
+            LOGGER.log(Level.WARNING, "Invalid age input: {0}", scanner.next());
             System.out.print("Enter an int: ");
             scanner.next();
         }
         age = scanner.nextInt();
+        LOGGER.info("Valid age entered.");
 
         scanner.close();
 
         final Person person = new Person(name, age);
+        LOGGER.info("Person created.");
 
         System.out.println(person.getName());
         System.out.println(person.getAge());
+
+        LOGGER.info("End applicaiton.");
+
+
     }
 }
